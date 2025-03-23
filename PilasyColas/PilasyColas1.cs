3e034d0fbff62dfa91ed98cc4f76b40846fa9bc9
Luis Aguero <l.aguero.2@estudiantec.cr>
2025-03-22 22:10:34 -0600

    subir archivos

---
+++ PilasyColas/PilasyColas1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Common interface for stacks
+public interface IStack
+{
+    void Push(int element);
+    int Pop();
+    int Top();
+    bool IsEmpty();
+}
+
+// Stack implementation using an array
+public class ArrayStack : IStack
+{
+    private int[] stack;
+    private int top;
+
+    public ArrayStack(int size)
+    {
+        stack = new int[size];
+        top = -1;
+    }
+
+    public void Push(int element)
+    {
+        if (top == stack.Length - 1)
+            throw new InvalidOperationException("Stack Overflow");
+        stack[++top] = element;
+    }
+
+    public int Pop()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack Underflow");
+        return stack[top--];
+    }
+
+    public int Top()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack Underflow");
+        return stack[top];
+    }
+
+    public bool IsEmpty()
+    {
+        return top == -1;
+    }
+}
+
+// Stack implementation using a linked list
+public class LinkedListStack : IStack
+{
+    private class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int data) { Data = data; Next = null; }
+    }
+
+    private Node top;
+
+    public void Push(int element)
+    {
+        Node newNode = new Node(element);
+        newNode.Next = top;
+        top = newNode;
+    }
+
+    public int Pop()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack Underflow");
+        int data = top.Data;
+        top = top.Next;
+        return data;
+    }
+
+    public int Top()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack Underflow");
+        return top.Data;
+    }
+
+    public bool IsEmpty()
+    {
+        return top == null;
+    }
+}
+
+// Common interface for queues
+public interface IQueue
+{
+    void Enqueue(int element);
+    int Dequeue();
+    int Front();
+    bool IsEmpty();
+}
+
+// Queue implementation using an array
+public class ArrayQueue : IQueue
+{
+    private int[] queue;
+    private int front;
+    private int rear;
+
+    public ArrayQueue(int size)
+    {
+        queue = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    public void Enqueue(int element)
+    {
+        if (rear == queue.Length - 1)
+            throw new InvalidOperationException("Queue Overflow");
+        if (front == -1) front = 0;
+        queue[++rear] = element;
+    }
+
+    public int Dequeue()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue Underflow");
+        int element = queue[front];
+        if (front == rear) front = rear = -1;
+        else front++;
+        return element;
+    }
+
+    public int Front()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue Underflow");
+        return queue[front];
+    }
+
+    public bool IsEmpty()
+    {
+        return front == -1;
+    }
+}
+
+// Queue implementation using a linked list
+public class LinkedListQueue : IQueue
+{
+    private class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int data) { Data = data; Next = null; }
+    }
+
+    private Node front;
+    private Node rear;
+
+    public void Enqueue(int element)
+    {
+        Node newNode = new Node(element);
+        if (rear == null)
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear.Next = newNode;
+            rear = newNode;
+        }
+    }
+
+    public int Dequeue()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue Underflow");
+        int data = front.Data;
+        front = front.Next;
+        if (front == null) rear = null;
+        return data;
+    }
+
+    public int Front()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue Underflow");
+        return front.Data;
+    }
+
+    public bool IsEmpty()
+    {
+        return front == null;
+    }
+}
--- TestPilasyColas/Test1.cs
+++ TestPilasyColas/Test1.cs
@@ -1,11 +1,149 @@
-﻿namespace TestPilasyColas
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class EstructurasDeDatosTests
 {
-    [TestClass]
-    public sealed class Test1
+    // Pruebas para la Pila con Arreglo
+    [TestMethod]
+    public void TestPilaConArreglo_Apilar()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        ArrayStack stack = new ArrayStack(5);
+        stack.Push(10);
+        Assert.AreEqual(10, stack.Top());
+    }
+
+    [TestMethod]
+    public void TestPilaConArreglo_Desapilar()
+    {
+        ArrayStack stack = new ArrayStack(5);
+        stack.Push(10);
+        stack.Push(20);
+        Assert.AreEqual(20, stack.Pop());
+        Assert.AreEqual(10, stack.Top());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestPilaConArreglo_DesapilarVacia()
+    {
+        ArrayStack stack = new ArrayStack(5);
+        stack.Pop(); // Debería lanzar excepción
+    }
+
+    [TestMethod]
+    public void TestPilaConArreglo_EstaVacia()
+    {
+        ArrayStack stack = new ArrayStack(5);
+        Assert.IsTrue(stack.IsEmpty());
+        stack.Push(10);
+        Assert.IsFalse(stack.IsEmpty());
+    }
+
+    // Pruebas para la Pila con Lista Enlazada
+    [TestMethod]
+    public void TestPilaConListaEnlazada_Apilar()
+    {
+        LinkedListStack stack = new LinkedListStack();
+        stack.Push(10);
+        Assert.AreEqual(10, stack.Top());
+    }
+
+    [TestMethod]
+    public void TestPilaConListaEnlazada_Desapilar()
+    {
+        LinkedListStack stack = new LinkedListStack();
+        stack.Push(10);
+        stack.Push(20);
+        Assert.AreEqual(20, stack.Pop());
+        Assert.AreEqual(10, stack.Top());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestPilaConListaEnlazada_DesapilarVacia()
+    {
+        LinkedListStack stack = new LinkedListStack();
+        stack.Pop(); // Debería lanzar excepción
+    }
+
+    [TestMethod]
+    public void TestPilaConListaEnlazada_EstaVacia()
+    {
+        LinkedListStack stack = new LinkedListStack();
+        Assert.IsTrue(stack.IsEmpty());
+        stack.Push(10);
+        Assert.IsFalse(stack.IsEmpty());
+    }
+
+    // Pruebas para la Cola con Arreglo
+    [TestMethod]
+    public void TestColaConArreglo_Encolar()
+    {
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.Enqueue(10);
+        Assert.AreEqual(10, queue.Front());
+    }
+
+    [TestMethod]
+    public void TestColaConArreglo_Desencolar()
+    {
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        Assert.AreEqual(10, queue.Dequeue());
+        Assert.AreEqual(20, queue.Front());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestColaConArreglo_DesencolarVacia()
+    {
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.Dequeue(); // Debería lanzar excepción
+    }
+
+    [TestMethod]
+    public void TestColaConArreglo_EstaVacia()
+    {
+        ArrayQueue queue = new ArrayQueue(5);
+        Assert.IsTrue(queue.IsEmpty());
+        queue.Enqueue(10);
+        Assert.IsFalse(queue.IsEmpty());
+    }
+
+    // Pruebas para la Cola con Lista Enlazada
+    [TestMethod]
+    public void TestColaConListaEnlazada_Encolar()
+    {
+        LinkedListQueue queue = new LinkedListQueue();
+        queue.Enqueue(10);
+        Assert.AreEqual(10, queue.Front());
+    }
+
+    [TestMethod]
+    public void TestColaConListaEnlazada_Desencolar()
+    {
+        LinkedListQueue queue = new LinkedListQueue();
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        Assert.AreEqual(10, queue.Dequeue());
+        Assert.AreEqual(20, queue.Front());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestColaConListaEnlazada_DesencolarVacia()
+    {
+        LinkedListQueue queue = new LinkedListQueue();
+        queue.Dequeue(); // Debería lanzar excepción
+    }
+
+    [TestMethod]
+    public void TestColaConListaEnlazada_EstaVacia()
+    {
+        LinkedListQueue queue = new LinkedListQueue();
+        Assert.IsTrue(queue.IsEmpty());
+        queue.Enqueue(10);
+        Assert.IsFalse(queue.IsEmpty());
     }
 }
